2a612557e2135f895d9adbf472526b027bc57192
Laughing6901 <vandat96tn@gmail.com>
2021-08-19 03:44:33 +0700

    fix server

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 5500
+const PORT = process.env.PORT || 5500;
 
-app.use(express.static('public'))
 app.use(express.static('files'))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile('./public/index.html');
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+module.exports = app;
